44bd52d23671ab19b551b1bfcb171465cd491354
Cristian Guerrero <guerrero.chavez.cristia@gmail.com>
2020-01-28 13:26:44 -0500

    min test

--- 2test.py
+++ 2test.py
@@ -2,6 +2,22 @@ from scapy.all import *
 from scapy.layers.inet import IP, TCP
 from scapy.layers.l2 import ARP
 
+import platform
+
+
+print (platform.system() )
+
+
+import psutil
+
+addrs = psutil.net_if_addrs()
+print(addrs.keys())
+
+
+# netifaces==0.10.8
+# psutil==5.6.7
+
+
 
 def arp_monitor_callback(pkt):
   if ARP in pkt and pkt[ARP].op in (1, 2):  # who-has or is-at
@@ -19,4 +35,5 @@ def packet_callback(packet):
                                                  str(bytes(packet[TCP].payload))))
 
 
-sniff(iface='en0', filter="tcp", prn=packet_callback, store=0)
+sniff( filter="tcp", prn=packet_callback, store=0, )
+#sniff(iface='en0', filter="tcp", prn=packet_callback, store=0)
